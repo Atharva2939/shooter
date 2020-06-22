5da7fa72353560078c0ecbac1c2a80a89522307f
Atharva2939 <63406858+Atharva2939@users.noreply.github.com>
2020-06-22 20:17:40 +0530

    Add files via upload

---
+++ box.js
@@ -0,0 +1,27 @@
+// class Box {
+//   constructor() {
+//     var options = {
+//         // 'restitution':0.8,
+//         // 'friction':0.3,
+//         // 'density':1.0
+//         'isStatic':true
+//     }
+  
+//     this.body = Bodies.(x, y,radius, options);
+//     // this.width = width;
+//     // this.height = height;
+    
+//     World.add(world, this.body);
+//   }
+//   display(){
+//     // var pos =this.body.position;
+//     // var angle=this.body.angle;
+//     // push();
+//     // translate(pos.x,pos.y);
+//     // rotate(angle);
+//     // rectMode(CENTER);
+//     fill(255);
+//     ellipse(, 0, this.width, this.height);
+//     //pop();
+//   }
+// };
